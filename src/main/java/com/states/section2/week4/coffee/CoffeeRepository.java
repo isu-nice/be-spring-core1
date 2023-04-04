b2cc5748a1534cc61ef38c2dea5afc451ef985dd
isu-nice <isu010614@naver.com>
2023-04-04 17:27:41 +0900

    test: coffee repository 객체 생성

---
+++ src/main/java/com/states/section2/week4/coffee/CoffeeRepository.java
@@ -0,0 +1,30 @@
+package com.states.section2.week4.coffee;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CoffeeRepository {
+
+    private static Map<Long, Coffee> drinks = new HashMap<>();
+
+    public void postCoffee(Coffee coffee) {
+        drinks.put(coffee.getCoffeeId(), coffee);
+    }
+
+    public Coffee patchCoffee(Long coffeeId, String korName, int price) {
+        Coffee drink = drinks.get(coffeeId);
+        drink.setKorName(korName);
+        drink.setPrice(price);
+
+        return drinks.put(coffeeId, drink);
+    }
+
+    public Coffee getCoffee(Long coffeeId) {
+        return drinks.get(coffeeId);
+    }
+
+    public void deleteCoffee(Long coffeeId) {
+        drinks.remove(coffeeId);
+    }
+
+}
