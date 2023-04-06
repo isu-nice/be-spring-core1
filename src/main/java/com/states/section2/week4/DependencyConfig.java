3cf9b129ea3983b4fbae986b7e606a81f0befa0f
isu-nice <isu010614@naver.com>
2023-04-06 10:07:01 +0900

    refactor: 애너테이션 추가

--- src/main/java/com/states/section2/week4/DependencyConfig.java
+++ src/main/java/com/states/section2/week4/DependencyConfig.java
@@ -4,20 +4,27 @@ import com.states.section2.week4.coffee.CoffeeRepository;
 import com.states.section2.week4.coffee.CoffeeService;
 import com.states.section2.week4.member.MemberRepository;
 import com.states.section2.week4.member.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DependencyConfig {
+    @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemberRepository();
     }
 
+    @Bean
     public CoffeeService coffeeService() {
         return new CoffeeService(coffeeRepository());
     }
 
+    @Bean
     public CoffeeRepository coffeeRepository() {
         return new CoffeeRepository();
     }
