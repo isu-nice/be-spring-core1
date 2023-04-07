48129256fc51f9eaebf18df5860d926a5c5d38ce
isu-nice <isu010614@naver.com>
2023-04-07 14:03:19 +0900

    feat: bean definition 연습

---
+++ src/main/java/com/states/section2/week4/BeanDefinitionTest.java
@@ -0,0 +1,22 @@
+package com.states.section2.week4;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanDefinitionTest {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ac
+                = new AnnotationConfigApplicationContext(DependencyConfig.class);
+
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName +
+                        ", beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+}
